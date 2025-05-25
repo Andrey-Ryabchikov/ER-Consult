21e5486a72850601ed0885cfa41d0ee25614c43a
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2025-05-25 15:42:20 +0300

    MVP2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-const modal = document.getElementById('serviceModal');
-const modalTitle = document.getElementById('modalTitle');
-const modalDescription = document.getElementById('modalDescription');
-const closeModal = document.querySelector('.close');
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
 
-document.querySelectorAll('.service-card .btn').forEach(button => {
-  button.addEventListener('click', function() {
-    const card = this.parentElement;
-    modalTitle.textContent = card.querySelector('h3').textContent;
-    modalDescription.textContent = card.querySelector('p').textContent;
-    modal.style.display = 'flex';
-  });
-});
-
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-});
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active');
+})
