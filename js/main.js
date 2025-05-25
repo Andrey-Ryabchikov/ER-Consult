09fa616326332d29ede446e81706bd50aba8d64b
Andrey-Ryabchikov <a_ryabchikov@inbox.ru>
2025-05-25 16:50:17 +0300

    MVP2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector('.menu__btn');
 const menu = document.querySelector('.menu__list');
 
+
+window.addEventListener('DOMContentLoaded', () => {
+    menu.classList.remove('menu--active');
+});
+
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('menu--active');
-})
+});
